4aca4360407a3e480f5f52c87bade3c12a2b1205
Raymond T. Hightower <rth@rayhightower.com>
2018-10-24 17:28:20 -0700

    Build the equivalent of hello-world for a web server.

--- project.clj
+++ project.clj
@@ -1,7 +1,10 @@
 (defproject clojure-simple-http "0.1.0-SNAPSHOT"
   :author "Raymond T. Hightower"
   :description "FIXME: write description"
+  :min-lein-version "2.7.1"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [htp-kit "2.2.0"]]
+  :main clojure-simple-http.core)
--- src/clojure_simple_http/core.clj
+++ src/clojure_simple_http/core.clj
@@ -1,4 +1,15 @@
-(ns clojure-simple-http.core)
+(ns clojure-simple-http.core
+  (:require [org.httpkit.server :refer [run-server]]
+            [clj-time.core :as t]))
+
+(defn app [req]
+  {:status 200
+   :headers {"Content-Ty[e" "text/html"}
+   :body (str (t/time-now))})
+
+(defn -main [& args]
+  (run-server app {:port 8080})
+  (println "Server started on port 8080))
 
 (defn foo
   "I don't do a whole lot."
