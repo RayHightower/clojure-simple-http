6f38a29545470f3f64f727ac8d39bf006bf4d4ab
Raymond T. Hightower <rth@rayhightower.com>
2018-10-24 17:32:50 -0700

    Typos!

--- project.clj
+++ project.clj
@@ -6,5 +6,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [htp-kit "2.2.0"]]
+                 [http-kit "2.2.0"]]
   :main clojure-simple-http.core)
