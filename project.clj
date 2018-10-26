71c5c929dc5463428a87cfb2ea3cb4d5dbea87ab
Raymond T. Hightower <rth@rayhightower.com>
2018-10-26 08:26:11 -0700

    Review project.clj and a prep a list of DFQs.

--- project.clj
+++ project.clj
@@ -2,9 +2,10 @@
   :author "Raymond T. Hightower"
   :description "FIXME: write description"
   :min-lein-version "2.7.1"
-  :url "http://example.com/FIXME"
+  ; :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [http-kit "2.2.0"]]
+  :jvm-opts ["--add-modules" "java.xml.bind"] ; Other folks have the same issue? https://github.com/bhauman/lein-figwheel/issues/612
   :main clojure-simple-http.core)
