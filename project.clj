c628b0e793ab494038bb54204e193b6fd0640e40
Raymond T. Hightower <rth@rayhightower.com>
2018-10-26 09:35:47 -0700

    Dependencies were missing. Fixed? Let us see!

--- project.clj
+++ project.clj
@@ -1,11 +1,12 @@
 (defproject clojure-simple-http "0.1.0-SNAPSHOT"
   :author "Raymond T. Hightower"
-  :description "FIXME: write description"
+  :description "Simple HTTP Server, a Clojure Experiment"
   :min-lein-version "2.7.1"
   ; :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [http-kit "2.2.0"]]
-  :jvm-opts ["--add-modules" "java.xml.bind"] ; Other folks have the same issue? https://github.com/bhauman/lein-figwheel/issues/612
+                 [http-kit "2.2.0"]
+                 [clj-time "0.14.0"]
+                 [compojure "1.6.0"]]
   :main clojure-simple-http.core)
