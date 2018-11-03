ef80075a782dd56d2fb53d872462e713d903108e
Raymond T. Hightower <rth@rayhightower.com>
2018-11-03 08:52:14 -0700

    Before the next round of changes.

--- src/clojure_simple_http/core.clj
+++ src/clojure_simple_http/core.clj
@@ -14,7 +14,7 @@
 (defroutes app
            (GET "/" [] "<h1>This is only a test.</h1><br/><h3>Do not be alarmed!<br/>Now we know that the Clojure server works, even though it depends on a library called http-kit :-)</h3>")
            (GET "/get-time" [] (get-time))
-           (route/not-found "<h1>Page not found</h1>"))
+           (route/not-found "<h1>RTH's Magic 404 Page<br/>Page not found</h1>"))
 
 
 (defn -main [& args]
