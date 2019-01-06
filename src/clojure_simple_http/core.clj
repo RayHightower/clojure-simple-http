f0d46b283a0a8b007e3f509f85f3a4bd3a79f3a5
Raymond T. Hightower <rth@rayhightower.com>
2019-01-06 13:12:29 -0800

    A few experiments with the Clojure web server.

--- src/clojure_simple_http/core.clj
+++ src/clojure_simple_http/core.clj
@@ -1,5 +1,5 @@
 (ns clojure-simple-http.core
-  (:require [clojure-test :refer [is testing with-test deftest run-tests]]
+  (:require [clojure.test :refer [is testing with-test deftest run-tests]]
             [org.httpkit.server :refer [run-server]]
             [clj-time.core :as t]
             [compojure.core :refer :all]
@@ -13,9 +13,9 @@
     response))
 
 (defroutes app
-           (GET "/" [] "<h1>This is only a test.</h1><br/><h3>Do not be alarmed!<br/>Now we know that the Clojure server works, even though it depends on a library called http-kit :-)</h3>")
+           (GET "/" [] "<h1>This is only a test.</h1><br/><h3>Do not be alarmed!<br/>Now we know that the Clojure server works, even though it depends on a library called http-kit :-)<br/><a href=\"/get-time\">What time is it?</a><br/><a href=\"/not-found\">What does the 404 page look like?</a></h3>")
            (GET "/get-time" [] (get-time))
-           (route/not-found "<h1>RTH's Magic 404 Page<br/>Page not found</h1>"))
+           (route/not-found "<h1>RTH's Magic 404 Page<br/>Page not found</h1><br/><br/><a href=\"/\">Back home.</a>"))
 
 
 (defn -main [& args]
